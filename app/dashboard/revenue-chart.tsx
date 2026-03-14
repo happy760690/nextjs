aadfdd9be8cc563c3dee518041091f2eb73bbd08
happy760690 <glitteringma@gmail.com>
2026-03-14 20:49:25 +0800

    striming a component

--- app/dashboard/revenue-chart.tsx
+++ app/dashboard/revenue-chart.tsx
@@ -5,6 +5,7 @@ import { fetchRevenue } from '@/app/lib/data';
 
 export default async function RevenueChart() {
   const revenue = await fetchRevenue();
+
   const chartHeight = 350;
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
@@ -19,6 +20,7 @@ export default async function RevenueChart() {
       </h2>
       <div className="rounded-xl bg-gray-50 p-4">
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
+          {/* y-axis */}
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
@@ -28,6 +30,7 @@ export default async function RevenueChart() {
             ))}
           </div>
 
+          {/* bars */}
           {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
@@ -35,7 +38,7 @@ export default async function RevenueChart() {
                 style={{
                   height: `${(chartHeight / topLabel) * month.revenue}px`,
                 }}
-              ></div>
+              />
               <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
                 {month.month}
               </p>
@@ -50,3 +53,4 @@ export default async function RevenueChart() {
     </div>
   );
 }
+ 
